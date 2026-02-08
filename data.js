f6833c12d98fcfbad00f02a21d63e7a7000b3116
abroryt9-cloud <abroryt9@gmail.com>
2026-02-08 17:31:12 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,26 @@
+const products = [
+    {
+        name: "Classic White",
+        description: "Идеальная белая рубашка для деловых и вечерних образов.",
+        price: "12 900 ₽",
+        image: "images/shirt1.jpg"
+    },
+    {
+        name: "Midnight Blue",
+        description: "Глубокий синий цвет и безупречная посадка.",
+        price: "13 500 ₽",
+        image: "images/shirt2.jpg"
+    },
+    {
+        name: "Graphite",
+        description: "Темно-серый оттенок для строгого стиля.",
+        price: "13 200 ₽",
+        image: "images/shirt3.jpg"
+    },
+    {
+        name: "Ivory",
+        description: "Мягкий оттенок слоновой кости для утончённого образа.",
+        price: "14 000 ₽",
+        image: "images/shirt4.jpg"
+    }
+];
